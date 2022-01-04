60b3bc96406e32401ca2a0950fc201537b30e897
rubenhillo <rho5991388@hotmail.com>
2022-01-03 22:09:19 -0600

    Agregando app.js modificado

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
-
-
-const publicFolderPath = path.resolve(__dirname,'./public');
+const mainController = require('./controllers/mainController');
+const publicFolderPath = path.resolve(__dirname, './public');
 const app = express();
+const routes = require('./routers/main');
+app.set('view engine', 'ejs');
 app.use(express.static(publicFolderPath));
+app.use('/', routes);
 
 app.listen(process.env.PORT || '3000', function(){
     console.log("Servidor iniciado en el puerto 3000")
 });
 
-
+/*
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/views/home.html')
 });
@@ -18,3 +20,4 @@ app.get('/',(req,res)=>{
 app.get('/detalle',(req,res)=>{
     res.sendFile(__dirname + '/views/productdetail.html')
 });
+*/
