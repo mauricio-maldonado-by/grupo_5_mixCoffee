6d8b01b30c35731a23eea6e346b0395883dc2637
rubenhillo <rho5991388@hotmail.com>
2022-03-18 23:43:20 -0600

    prueba

--- src/routers/main.js
+++ src/routers/main.js
@@ -4,8 +4,6 @@ const publicFolderPath = path.resolve(__dirname, './public');
 const mainController = require('../controllers/mainController');
 const router = express.Router();
  
-//router.get('/search', mainController.search); 
-
 router.use(express.static(publicFolderPath));
 router.get('/', mainController.main);
 router.get ('/home', mainController.main);
