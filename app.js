51e115429602c18f0b8edcfcb6a69822d1f9f95a
rubenhillo <rho5991388@hotmail.com>
2022-03-30 22:13:57 -0600

    CRUD - Editar - corregido

--- app.js
+++ app.js
@@ -5,23 +5,20 @@ const app = express();
 const mainRouter = require('./src/routers/main'); 
 const productsRouter = require('./src/routers/products');
 const methodOverride = require('method-override'); 
-app.use(methodOverride('_method'));
-
-
-app.use(express.static(publicFolderPath));
-app.use('/', mainRouter);
-app.use('/products', productsRouter);
-
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './src/views'));
+
+app.use(methodOverride('_method'));
+app.use(express.static(publicFolderPath));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/', mainRouter);
+app.use('/products', productsRouter);
+
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 
 app.listen(process.env.PORT || '3000', function(){
     console.log("Servidor iniciado en el puerto 3000")
